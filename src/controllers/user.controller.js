be9fda4c824b601db992f9796a556e3dc3516b4c
vivek thakur <vivekthakur99000@gmail.com>
2024-08-07 13:59:52 +0530

    "Update backend subproject commit reference"

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -398,6 +398,57 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+  const  user  = await User.aggregate([
+    {
+      $match : {
+        _id : new mongoose.Types.ObjectId(req.user._id)
+      }
+    }, 
+    {
+      $lookup : {
+        from : "videos",
+        localField : "watchhistory",
+        foreignField : "_id",
+        as : "watchHistory",
+        pipeline : [
+          {
+            $lookup : {
+              from : "users",
+              localField : "owner",
+              foreignField : "_id",
+              as : "owner",
+              pipeline : [
+                {
+                  $project : {
+                    fullname : 1,
+                    username : 1,
+                    avatar : 1,
+                  }
+                }
+              ]
+            }
+          }, {
+            $addField : {
+              owner : {
+                $first : "$owner"
+              }
+            }
+          }
+        ]
+    }
+  }
+  ])
+
+  return res.status(200)
+.json(new apiResponse(200,
+  user[0].watchhistory,
+  "watch history fetched successfully"
+))
+})
+
+
+
 export {
   registerUser,
   loginUser,
@@ -409,4 +460,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
